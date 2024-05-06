1366d90604445a47b4f2996140a7af1756f73f8a
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-06 23:14:35 +0900

    feat: 테스트를 작성했지만 문제가 많은 테스트다

--- src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
+++ src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
@@ -2,12 +2,25 @@ package sample.cafekiosk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import sample.cafekiosk.unit.CafeKiosk;
+import sample.cafekiosk.unit.beverage.Americano;
 
-@SpringBootTest
+
 class CafekioskApplicationTests {
 
     @Test
-    void contextLoads() {
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+
     }
+
+    /*** 해당 테스트의 문제점 - 수동화된 테스트
+     *  1. 최종 단계에 사람이 개입해야하는 문제점 (자동화된 테스트가 아님)
+     *  2. 다른 사람이 해당 테스트를 봤을 때, 어떤게 맞는건지 어떤게 틀린건지 알 수 없음 - 무조건 성공하는 테스트
+     */
 
 }
