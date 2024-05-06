a43f220a614366525ba5115ab83e955bd7528f02
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-06 22:46:48 +0900

    feat: 요구사항을 반영하기 위해 메모리상에 add, remove, clear 구현

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,8 +1,21 @@
 package sample.cafekiosk.unit;
 
-public class CafeKiosk {
-    public void add(Beverage beverage) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
     }
 
     public int calculateTotalPrice() {
