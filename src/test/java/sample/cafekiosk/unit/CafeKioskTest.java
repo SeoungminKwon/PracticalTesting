c9d0601078ee7bbd2e2cd70a71e52f08a8f20a48
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-11 18:22:28 +0900

    test: 10시로 LocalDateTime만들어서 test진행

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
@@ -77,6 +78,19 @@ class CafeKioskTest {
 
         cafeKiosk.add(americano);
         Order order = cafeKiosk.createOrder(); //가게 운영시간만 테스트 성공함
+
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void createOrderWithCurrentTime() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        //10시 경계값
+        Order order = cafeKiosk.createOrder(LocalDateTime.of(2024, 5, 11, 10, 0)); //가게 운영시간만 테스트 성공함
 
         assertThat(order.getBeverages()).hasSize(1);
         assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
