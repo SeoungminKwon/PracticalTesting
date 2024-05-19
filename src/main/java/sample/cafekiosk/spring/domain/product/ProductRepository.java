1646d653c034c7202766e78164a5fa09d54ddf2c
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-19 20:52:33 +0900

    feat: 상품번호로 Product를 조회한다.

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -14,4 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
      */
     List<Product> findALlBySellingStatusIn(List<ProductSellingType> sellingTypes);
 
+    List<Product> findALlByProductNumberIn(List<String> productNumbers);
+
 }
