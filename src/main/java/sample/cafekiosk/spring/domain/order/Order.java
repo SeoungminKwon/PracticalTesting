0b44583b46869bf8cd90336210ab0f63ac65ef0e
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-19 19:47:24 +0900

    feat: OrderProductEntity 생성후 Order과 양방향 연관관계

--- src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -1,17 +1,22 @@
 package sample.cafekiosk.spring.domain.order;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProduct;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,4 +34,8 @@ public class Order extends BaseEntity {
     private int totalPrice;
 
     private LocalDateTime registeredDateTime;
+
+    //주문쪽에선 필요해서 양방향 관계
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
 }
