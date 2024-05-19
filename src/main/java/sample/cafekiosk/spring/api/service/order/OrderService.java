24c5c775b6ce990e94d110dadbeed92d3bce0c99
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-19 22:46:00 +0900

    refactor: createOrder() 리펙토링

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -23,18 +23,22 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         //Product
-        List<Product> products = productRepository.findALlByProductNumberIn(productNumbers); //productNumbers에 "001", "001"로 들어가도 IN절로 중복 제거가 됨
-        Map<String, Product> productMap = products.stream()
-                                               .collect(Collectors.toMap(Product::getProductNumber, p -> p));
-        List<Product> duplicateProducts = productNumbers.stream()
-                                                  .map(productMap::get)
-                                                  .toList();
-
+        List<Product> products = findProductsBy(productNumbers);
 
         //Order
-        Order order = Order.create(duplicateProducts, registeredDateTime);
+        Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder); //ID값이 DB에 저장된 이후에 PK값으로 발급받음
+    }
+
+    private List<Product> findProductsBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findALlByProductNumberIn(productNumbers); //productNumbers에 "001", "001"로 들어가도 IN절로 중복 제거가 됨
+        Map<String, Product> productMap = products.stream()
+                                               .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        return productNumbers.stream()
+                                                  .map(productMap::get)
+                                                  .toList();
     }
 }
