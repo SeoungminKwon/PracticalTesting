c0ed9fd87355cb43fc4e507701912258a4fb0ca0
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-19 22:41:52 +0900

    fix: 중복되는 상품번호 리스트로 주문 생성

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.order;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
@@ -21,10 +23,16 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         //Product
-        List<Product> products = productRepository.findALlByProductNumberIn(productNumbers);
+        List<Product> products = productRepository.findALlByProductNumberIn(productNumbers); //productNumbers에 "001", "001"로 들어가도 IN절로 중복 제거가 됨
+        Map<String, Product> productMap = products.stream()
+                                               .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+        List<Product> duplicateProducts = productNumbers.stream()
+                                                  .map(productMap::get)
+                                                  .toList();
+
 
         //Order
-        Order order = Order.create(products, registeredDateTime);
+        Order order = Order.create(duplicateProducts, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder); //ID값이 DB에 저장된 이후에 PK값으로 발급받음
