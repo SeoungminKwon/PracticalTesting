6fd91cf3bb6072373e5beebdc7eca4880c7c9189
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-05-19 20:31:50 +0900

    feat: TDD 방식으로 Order서비스 개발 - RED

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -2,7 +2,6 @@ package sample.cafekiosk.spring.domain.product;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingType.*;
 import static sample.cafekiosk.spring.domain.product.ProductType.*;
 
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test") //test 코드 실행시 TEST profile로 실행이됨
